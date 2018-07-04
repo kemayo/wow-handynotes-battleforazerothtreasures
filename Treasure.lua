37ab5dd9bc39d7762e4384682267119725274eba
David Lynch <kemayo@gmail.com>
2018-07-04 16:14:40 -0500

    Add some trailing commas

--- Treasure.lua
+++ Treasure.lua
@@ -46,15 +46,15 @@ ns.points = {
     },
     --]]
     [862] = { -- Zuldazar
-        [54093150] = {quest=48938, achievement=12851, criteria=40988, note="On second floor"}, -- Offerings of the Chosen
-        [51718690] = {quest=49936, achievement=12851, criteria=40990, note="Bottom floor of ship"}, -- Spoils of Pandaria
-        [49486526] = {quest=49257, achievement=12851, criteria=40992, note="Top of ship"}, -- Warlord's Cache
-        [61065863] = {quest=50947, achievement=12851, criteria=40994, npc=133208, note="Event: kill Da White Shark first"}, -- Da White Shark's Bounty
-        [56123806] = {quest=51338, achievement=12851, criteria=40996, note="In cave behind waterfall"}, -- Cache of Secrets
-        [64732170] = {quest=50259, achievement=12851, criteria=40989}, -- Witch Doctor's Hoard
-        [51432661] = {quest=50582, achievement=12851, criteria=40991}, -- Gift of the Brokenhearted
-        [38793443] = {quest=50707, achievement=12851, criteria=40993, note="Road behind waterfall"}, -- Dazar's Forgotten Chest
-        [71841676] = {quest=50949, achievement=12851, criteria=40995}, -- The Exile's Lament
+        [54093150] = {quest=48938, achievement=12851, criteria=40988, note="On second floor",}, -- Offerings of the Chosen
+        [51718690] = {quest=49936, achievement=12851, criteria=40990, note="Bottom floor of ship",}, -- Spoils of Pandaria
+        [49486526] = {quest=49257, achievement=12851, criteria=40992, note="Top of ship",}, -- Warlord's Cache
+        [61065863] = {quest=50947, achievement=12851, criteria=40994, npc=133208, note="Event: kill Da White Shark first",}, -- Da White Shark's Bounty
+        [56123806] = {quest=51338, achievement=12851, criteria=40996, note="In cave behind waterfall",}, -- Cache of Secrets
+        [64732170] = {quest=50259, achievement=12851, criteria=40989,}, -- Witch Doctor's Hoard
+        [51432661] = {quest=50582, achievement=12851, criteria=40991,}, -- Gift of the Brokenhearted
+        [38793443] = {quest=50707, achievement=12851, criteria=40993, note="Road behind waterfall",}, -- Dazar's Forgotten Chest
+        [71841676] = {quest=50949, achievement=12851, criteria=40995,}, -- The Exile's Lament
         [71161767] = path(50949),
         [52974722] = {quest=51624, achievement=12851, criteria=40997}, -- Riches of Tor'nowa
     },
@@ -62,11 +62,11 @@ ns.points = {
         -- [] = {quest=, achievement=, criteria=, note=""}, -- 
     },
     [864] = { -- Vol'dun
-        -- [] = {quest=, achievement=, criteria=, note=""}, -- 
+        -- [] = {quest=, achievement=, criteria=, note="",}, -- 
     },
     [895] = { -- Tiragarde Sound
         [61515233] = {quest=49963, achievement=12852, criteria=41012, note="Ride the Guardian",}, -- Hay Covered Chest
-        [56033319] = {quest=52866, achievement=12852, criteria=41014, note="",}, -- Precarious Noble Cache
+        [56033319] = {quest=52866, achievement=12852, criteria=41014,}, -- Precarious Noble Cache
         [72482169] = {quest=52870, achievement=12852, criteria=41016, note="In cave",}, -- Scrimshaw Cache
         [54994608] = {quest=52807, achievement=12852, criteria=41018, note="Kill pirates in Freehold until the map drops",}, -- Soggy Treasure Map
         [90507551] = {quest=52836, achievement=12852, criteria=41020, note="Kill pirates in Freehold until the map drops",}, -- Yellowed Treasure Map
@@ -77,27 +77,27 @@ ns.points = {
         -- [] = {quest=52195, achievement=12852, criteria=41017, note="",}, -- Secret of the Depths
     },
     [896] = { -- Drustvar
-        [33713008] = {quest=53356, achievement=12995, criteria=41697}, -- Web-Covered Chest
-        [63306585] = {quest=53385, achievement=12995, criteria=41699, note="Left Down Up Right"}, -- Runebound Cache
-        [33687173] = {quest=53387, achievement=12995, criteria=41701, note="Right Up Left Down"}, -- Runebound Coffer
-        [55605181] = {quest=53472, achievement=12995, criteria=41703, note="Click on Witch Torch"}, -- Bespelled Chest
-        [25472416] = {quest=53474, achievement=12995, criteria=41705, note="Click on Witch Torch"}, -- Enchanted Chest
-        [25751995] = {quest=53357, achievement=12995, criteria=41698, note="Get keys from Gorging Raven"}, -- Merchant's Chest
-        [44222770] = {quest=53386, achievement=12995, criteria=41700, note="Left Right Down Up"}, -- Runebound Chest
-        [18515133] = {quest=53471, achievement=12995, criteria=41702, note="Click on Witch Torch"}, -- Hexed Chest
-        [67767367] = {quest=53473, achievement=12995, criteria=41704, note="Click on Witch Torch"}, -- Ensorcelled Chest
+        [33713008] = {quest=53356, achievement=12995, criteria=41697,}, -- Web-Covered Chest
+        [63306585] = {quest=53385, achievement=12995, criteria=41699, note="Left Down Up Right",}, -- Runebound Cache
+        [33687173] = {quest=53387, achievement=12995, criteria=41701, note="Right Up Left Down",}, -- Runebound Coffer
+        [55605181] = {quest=53472, achievement=12995, criteria=41703, note="Click on Witch Torch",}, -- Bespelled Chest
+        [25472416] = {quest=53474, achievement=12995, criteria=41705, note="Click on Witch Torch",}, -- Enchanted Chest
+        [25751995] = {quest=53357, achievement=12995, criteria=41698, note="Get keys from Gorging Raven",}, -- Merchant's Chest
+        [44222770] = {quest=53386, achievement=12995, criteria=41700, note="Left Right Down Up",}, -- Runebound Chest
+        [18515133] = {quest=53471, achievement=12995, criteria=41702, note="Click on Witch Torch",}, -- Hexed Chest
+        [67767367] = {quest=53473, achievement=12995, criteria=41704, note="Click on Witch Torch",}, -- Ensorcelled Chest
         -- [] = {quest=53475, achievement=12995, criteria=41752, note=""}, -- Stolen Thornspeaker Cache
     },
     [942] = { -- Stormsong Valley
-        [53524173] = {quest=51449, achievement=12853, criteria=41061}, -- Weathered Treasure Chest
-        [50118623] = {quest=50526, achievement=12853, criteria=41063}, -- Frosty Treasure Chest
-        [59913907] = {quest=50937, achievement=12853, criteria=41065, note="On roof"}, -- Hidden Scholar's Chest
-        [58216368] = {quest=52326, achievement=12853, criteria=41067, note="Top shelf inside shed"}, -- Discarded Lunchbox
-        [36692323] = {quest=52976, achievement=12853, criteria=41069, note="Climb ladder onto ship"}, -- Venture Co. Supply Chest
-        [42854723] = {quest=50089, achievement=12853, criteria=41062, note="In cave"}, -- Old Ironbound Chest
-        [67224321] = {quest=50734, achievement=12853, criteria=41064, note="Under ship"}, -- Sunken Strongbox
-        [58608388] = {quest=49811, achievement=12853, criteria=41066, note="Under platform"}, -- Smuggler's Stash
-        [44447353] = {quest=52429, achievement=12853, criteria=41068, note="Jump onto platform"}, -- Carved Wooden Chest
-        [46003069] = {quest=52980, achievement=12853, criteria=41070, note="Behind pillar"}, -- Forgotten Chest
+        [53524173] = {quest=51449, achievement=12853, criteria=41061,}, -- Weathered Treasure Chest
+        [50118623] = {quest=50526, achievement=12853, criteria=41063,}, -- Frosty Treasure Chest
+        [59913907] = {quest=50937, achievement=12853, criteria=41065, note="On roof",}, -- Hidden Scholar's Chest
+        [58216368] = {quest=52326, achievement=12853, criteria=41067, note="Top shelf inside shed",}, -- Discarded Lunchbox
+        [36692323] = {quest=52976, achievement=12853, criteria=41069, note="Climb ladder onto ship",}, -- Venture Co. Supply Chest
+        [42854723] = {quest=50089, achievement=12853, criteria=41062, note="In cave",}, -- Old Ironbound Chest
+        [67224321] = {quest=50734, achievement=12853, criteria=41064, note="Under ship",}, -- Sunken Strongbox
+        [58608388] = {quest=49811, achievement=12853, criteria=41066, note="Under platform",}, -- Smuggler's Stash
+        [44447353] = {quest=52429, achievement=12853, criteria=41068, note="Jump onto platform",}, -- Carved Wooden Chest
+        [46003069] = {quest=52980, achievement=12853, criteria=41070, note="Behind pillar",}, -- Forgotten Chest
     },
 }
