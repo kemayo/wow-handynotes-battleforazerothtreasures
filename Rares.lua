fa9c79d546dd1d4e9654c84db454cdbca080be14
David Lynch <kemayo@gmail.com>
2018-08-14 17:23:29 -0500

    Rares for Nazmir

--- Rares.lua
+++ Rares.lua
@@ -9,6 +9,38 @@ end
 merge(ns.points[862], { -- Zuldazar
 })
 merge(ns.points[863], { -- Nazmir
+    [67812972] = {quest=48063, npc=125250, achievement=12942, criteria=41440,}, -- Ancient Jawbreaker
+    [32802690] = {quest=50563, npc=134293, achievement=12942, criteria=41447,}, -- Azerite-Infused Slag
+    [44224873] = {quest=49305, npc=128965, achievement=12942, criteria=41450,}, -- Uroku the Bound
+    [68102023] = {quest=50567, npc=134296, achievement=12942, criteria=41452,}, -- Chag's Challenge
+    [81813057] = {quest=48057, npc=125232, achievement=12942, criteria=41454,}, -- Cursed Chest
+    [68955747] = {quest=50361, npc=121242, achievement=12942, criteria=41456,}, -- Glompmaw
+    [56666932] = {quest=49312, npc=128974, achievement=12942, criteria=41458,}, -- Queen Tzxi'kik
+    [45375197] = {quest=50307, npc=133373, achievement=12942, criteria=41460,}, -- Jax'teb the Reanimated
+    [52931340] = {quest=47843, npc=124397, achievement=12942, criteria=41462,}, -- Kal'draxa
+    [81696105] = {quest=50565, npc=134295, achievement=12942, criteria=41464,}, -- Lost Scroll
+    [58963893] = {quest=48972, npc=127820, achievement=12942, criteria=41467,}, -- Scout Skrasniss
+    [31443815] = {quest=48508, npc=126460, achievement=12942, criteria=41469,}, -- Tainted Guardian
+    [38095768] = {quest=50888, npc=135565, achievement=12942, criteria=41472,}, -- Urn of Agussu
+    [48985082] = {quest=48623, npc=126907, achievement=12942, criteria=41474,}, -- Wardrummer Zurula
+    [38722674] = {quest=49469, npc=129657, achievement=12942, criteria=41476,}, -- Za'amar the Queen's Blade
+    [78084451] = {quest=50355, npc=133539, achievement=12942, criteria=41478,}, -- Lo'kuno
+    [54138091] = {quest=50569, npc=134298, achievement=12942, criteria=41444,}, -- Azerite-Infused Elemental
+    [43069033] = {quest=48541, npc=126635, achievement=12942, criteria=41448,}, -- Blood Priest Xak'lar
+    [53694287] = {quest=49317, npc=129005, achievement=12942, criteria=41451,}, -- King Kooba
+    [41665344] = {quest=48462, npc=126187, achievement=12942, criteria=41453,}, -- Corpse Bringer Yal'kar
+    [33538708] = {quest=48638, npc=127001, achievement=12942, criteria=41455,}, -- Gwugnug the Cursed
+    [32344332] = {quest=49231, npc=128426, achievement=12942, criteria=41457,}, -- Gutrip
+    [24967778] = {quest=47877, npc=124399, achievement=12942, criteria=41459,}, -- Infected Direhorn
+    [28003408] = {quest=50342, npc=133527, achievement=12942, criteria=41461,}, -- Juba the Scarred
+    [76033654] = {quest=48052, npc=125214, achievement=12942, criteria=41463,}, -- Krubbs
+    [42805949] = {quest=48439, npc=126142, achievement=12942, criteria=41466,}, -- Bajiatha
+    [58431014] = {quest=48980, npc=127873, achievement=12942, criteria=41468,}, -- Scrounger Patriarch
+    [49453714] = {quest=48406, npc=126056, achievement=12942, criteria=41470,}, -- Totem Maker Jash'ga
+    [29705107] = {quest=48626, npc=126926, achievement=12942, criteria=41473,}, -- Venomjaw
+    [36555053] = {quest=50348, npc=133531, achievement=12942, criteria=41475,}, -- Xu'ba
+    [38887148] = {quest=50423, npc=133812, achievement=12942, criteria=41477,}, -- Zanxib
+    [52605489] = {quest=50040, npc=128930, achievement=12942, criteria=41479,}, -- Mala'kili and Rohnkor
 })
 merge(ns.points[864], { -- Vol'dun
     [50378160] = {quest=nil, npc=135852, achievement=12943, criteria=41606,}, -- Ak'tar
