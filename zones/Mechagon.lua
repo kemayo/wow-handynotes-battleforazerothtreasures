60e44993e0959e918e65b4e8bbc5dddfc212702a
David Lynch <kemayo@gmail.com>
2022-04-07 09:09:05 -0500

    Secret Fish in mechagon

--- zones/Mechagon.lua
+++ zones/Mechagon.lua
@@ -112,3 +112,24 @@ ns.RegisterPoints(1462, { -- Mechagon
 }, {
     achievement=13470,
 })
+
+-- Secret Fish
+
+ns.RegisterPoints(1462, { -- Mechagon
+    [35004650] = { -- Bolted Steelhead, Energized Lighting Cod, Solarsprocket Barbel
+        criteria={44738, 44744, 44745},
+        loot={{167655,quest=55305},{167661,quest=55311},{167662,quest=55312}},
+        note="These can be fished anywhere on the coast",
+    },
+    [76704920] = {criteria=44737,loot={{167654,quest=55299}}}, -- Bottom Feeding Stinkfish
+    [55803190] = {criteria=44739,loot={{167656,quest=55306}}}, -- Pond Hopping Springfish
+    [59102280] = {criteria=44740,loot={{167657,quest=55307}}}, -- Shadowy Cave Eel
+    [26107570] = {criteria=44741,loot={{167658,quest=55308}}}, -- Mechanical Blowfish
+    [83707300] = {criteria=44742,loot={{167659,quest=55309}}}, -- Spitting Clownfish
+    [65505220] = {criteria=44743,loot={{167660,quest=55310}}}, -- Sludge-fouled Carp
+    [47403600] = {criteria=44746,loot={{167663,quest=55313}}}, -- Tasty Steelfin
+}, {
+    achievement=13489,
+    atlas="worldquest-icon-fishing",
+    minimap=true,
+})
